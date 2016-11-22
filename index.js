3a7876b153d2110dbf2b6754d752aa1e224ad664
nick <sbryant31@gmail.com>
2016-11-22 12:39:38 -0700

    version control public

--- index.js
+++ index.js
@@ -3,14 +3,11 @@ var static = require('node-static');
 //
 // Create a node-static server instance to serve the './public' folder
 //
-var file = new static.Server('');
+var file = new static.Server();
 var port = process.env.PORT || 8080;
 
 require('http').createServer(function (request, response) {
     request.addListener('end', function () {
-        //
-        // Serve files!
-        //
         file.serve(request, response);
     }).resume();
 }).listen(port);
