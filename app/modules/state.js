940de303fcc60ebf465da1b677620d6b082fdb09
Nathan Quarles <nateq314@gmail.com>
2017-01-03 11:22:35 -0500

    cleanup app/modules/state.js

--- app/modules/state.js
+++ app/modules/state.js
@@ -1,14 +1,13 @@
-var State = require('statesync')
-var lodash = require('lodash')
-var store = require('store')
+var State = require('statesync');
+var lodash = require('lodash');
+var store = require('store');
 
 var init = {
   token:store.get('token'),
   me:store.get('me'),
   org:store.get('org')
-}
+};
 
-var state = State(init,lodash.cloneDeep,function(){ return false })
+var state = State(init,lodash.cloneDeep,function(){ return false; });
 
-module.exports = state
-
+module.exports = state;
