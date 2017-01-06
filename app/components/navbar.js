d4159101d7e7396e5cef885ac07df361c2616883
Nathan Quarles <nateq314@gmail.com>
2017-01-06 15:22:43 -0500

    start to implement react-toolbox components

--- app/components/navbar.js
+++ app/components/navbar.js
@@ -1,40 +1,56 @@
-var React = require('react')
-var lodash = require('lodash')
-var actions = require('../modules/actions')
-import { browserHistory } from 'react-router'
-import { Colors } from '@blueprintjs/core'
+var React = require('react');
+var actions = require('../modules/actions');
+// import { Colors } from '@blueprintjs/core';
 
-var cloud = require('../img/cloud.png');
-var logo_text = require('../img/logo_text.png');
+import { AppBar } from 'react-toolbox/lib/app_bar';
+import Navigation from 'react-toolbox/lib/navigation';
+import Link from 'react-toolbox/lib/link';
+
+function logo() {
+  var cloud = require('../img/cloud.png');
+  var logo_text = require('../img/logo_text.png');
+  return (
+    <div>
+      <img src={cloud} />
+    </div>
+  );
+}
 
 module.exports = React.createClass({
   getDefaultProps:function(){
     return {
       links:[],
       avatarMenu: ''
-    }
+    };
   },
   render: function() {
-    var links = lodash.map(this.props.links,function(link){
-      return <li
-        key={link.name}
-        onClick={actions.goto.bind(null,link.url)}
-        className={'pt-menu-item ' + link.icon}
-        >{link.name}</li>
-    }.bind(this))
+    var links = this.props.links.map(l => (
+      <Link
+        key={l.name}
+        onClick={(e) => actions.goto(l.url, e)}
+        label={l.name}
+        href={l.url}
+        className={'pt-menu-item ' + l.icon}
+      />
+    ));
 
-    return <nav className='pt-navbar pt-fixed-top' style={{backgroundColor: '#16253d'}}>
-      <div className="pt-navbar-group pt-align-left">
-        <div className="pt-navbar-heading"><img src={cloud} style={{height: 45, width: 79}} /><img src={logo_text} style={{height: 35, width: 210}}/></div>
-      </div>
-      <div className="pt-navbar-group pt-align-right" >
-        <ul className='pt-menu' style={{backgroundColor: '#16253d', color: '#FFF'}}>
-          {links}
-          <span className="pt-navbar-divider"></span>
-          {this.props.children}
-        </ul>
-        {this.props.avatarMenu}
-      </div>
-    </nav>
+    return <AppBar title='nativeSync' leftIcon={logo()}>
+      <Navigation type='horizontal'>
+        {links}
+      </Navigation>
+    </AppBar>;
+    // return <nav className='pt-navbar pt-fixed-top' style={{backgroundColor: '#16253d'}}>
+    //   <div className="pt-navbar-group pt-align-left">
+    //     <div className="pt-navbar-heading"><img src={cloud} style={{height: 45, width: 79}} /><img src={logo_text} style={{height: 35, width: 210}}/></div>
+    //   </div>
+    //   <div className="pt-navbar-group pt-align-right" >
+    //     <ul className='pt-menu' style={{backgroundColor: '#16253d', color: '#FFF'}}>
+    //       {links}
+    //       <span className="pt-navbar-divider"></span>
+    //       {this.props.children}
+    //     </ul>
+    //     {this.props.avatarMenu}
+    //   </div>
+    // </nav>
   }
-})
+});
