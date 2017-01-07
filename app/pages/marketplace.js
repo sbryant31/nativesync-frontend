f70b71a140ad93cebc065db43031412e911f18c2
Nathan Quarles <nateq314@gmail.com>
2017-01-07 10:15:35 -0500

    tentative alpha version of Marketplace screen now working

--- app/pages/marketplace.js
+++ app/pages/marketplace.js
@@ -1,8 +1,5 @@
 var React = require('react');
-// var actions = require('../modules/actions');
-// var Navbar = require('../components/navbar');
 var IntegrationBrowser = require('../components/integration_browser');
-// var lodash = require('lodash');
 
 module.exports = React.createClass({
   getInitialState: function() {
