8099ab700d92bf40b8e40b99bd0456084235fcc7
Nathan Quarles <nateq314@gmail.com>
2017-01-07 10:30:39 -0500

    another minor change, separate flexboxgrid css classes from business logic classes

--- app/components/integration_browser/marketplace_view2.js
+++ app/components/integration_browser/marketplace_view2.js
@@ -7,23 +7,25 @@ module.exports = ({integrations = []}) => {
   return (
     <div className="row MarketplaceView">{
       integrations.map(integration => (
-        <div
-          key={integration.id}
-          className="integration pt-card pt-elevation-0 pt-interactive col-md-3"
-        >
-          <div className="services-icons">{
-            integration.Services.map((service, idx) => (
-              <span className="service" key={service.id}>
-                {idx > 0 ? <img className="circle_arrow" src={circle_arrow} /> : null}
-                <img src={service.logo_url} />
-              </span>
-            ))
-          }</div>
-          <h4 className="title">{integration.title}</h4>
-          <p className="description">{
-            integration.description ||
-            "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged."
-          }</p>
+        <div className="col-lg-3 col-md-4 col-sm-6 col-xs-12">
+          <div
+            key={integration.id}
+            className="integration pt-card pt-elevation-0 pt-interactive"
+          >
+            <div className="services-icons">{
+              integration.Services.map((service, idx) => (
+                <span className="service" key={service.id}>
+                  {idx > 0 ? <img className="circle_arrow" src={circle_arrow} /> : null}
+                  <img src={service.logo_url} />
+                </span>
+              ))
+            }</div>
+            <h4 className="title">{integration.title}</h4>
+            <p className="description">{
+              integration.description ||
+              "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged."
+            }</p>
+          </div>
         </div>
       ))
     }</div>
