d4758c67585354f809d2ddc6fbb61f024d151b86
Nathan Quarles <nateq314@gmail.com>
2017-01-07 13:28:44 -0500

    Marketplace page a little closer to final

--- app/components/integration_browser/marketplace_view2.js
+++ app/components/integration_browser/marketplace_view2.js
@@ -1,5 +1,6 @@
 var React = require('react');
 var actions = require('../../modules/actions');
+import EllipsisText  from 'react-ellipsis-text';
 
 const circle_arrow = require("../../img/circle_arrow.svg");
 
@@ -20,11 +21,25 @@ module.exports = ({integrations = []}) => {
                 </span>
               ))
             }</div>
+            {/* <div>{
+              integration.Services.map(service => (
+                <p>{service.name}</p>
+              ))
+            }</div> */}
             <h4 className="title">{integration.title}</h4>
-            <p className="description">{
-              integration.description ||
-              "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged."
-            }</p>
+            <EllipsisText
+              text={
+                integration.description ||
+                "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged."
+              }
+              length={'140'}
+            />
+            <div className="author">{`By ${integration.organization.name}`}</div>
+            {
+              Object.getOwnPropertyNames(integration.pricing).length > 0 ?
+                <div className="pricing">{`$${integration.pricing.upfront} + $${integration.pricing.monthly}/mo`}</div> :
+                ""
+            }
           </div>
         </div>
       ))
