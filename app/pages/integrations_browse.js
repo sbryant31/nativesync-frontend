f0a92b83db37584be130848bf40ea49b7cb7d468
nick <sbryant31@gmail.com>
2017-01-13 21:20:45 -0700

    fix integrations bug

--- app/pages/integrations_browse.js
+++ app/pages/integrations_browse.js
@@ -1,7 +1,7 @@
 var React = require('react')
 var actions = require('../modules/actions')
 var Navbar = require('../components/navbar')
-var IntegrationBrowser = require('../components/integration_browser/integration_browser');
+var IntegrationBrowser = require('../components/integration_browser/integration_browser.js');
 var lodash = require('lodash')
 
 module.exports = React.createClass({
