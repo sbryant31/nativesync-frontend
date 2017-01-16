41a0e075cfcaf9a2b6a877c6fcdbac7ebd3353de
nick <sbryant31@gmail.com>
2017-01-16 14:30:15 -0700

    remove upfront cost from marketplace

--- app/components/integration_browser/marketplace_view2.js
+++ app/components/integration_browser/marketplace_view2.js
@@ -30,7 +30,7 @@ module.exports = ({integrations = []}) => {
             <h4 className="title">{integration.title}</h4>
             <EllipsisText
               text={
-                integration.description ||
+                integration.description || integration.title ||
                 "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged."
               }
               length={140}
@@ -38,7 +38,7 @@ module.exports = ({integrations = []}) => {
             <div className="author">{`By ${integration.organization.name}`}</div>
             {
               Object.getOwnPropertyNames(integration.pricing).length > 0 ?
-                <div className="pricing">{`$${integration.pricing.upfront} + $${integration.pricing.monthly}/mo`}</div> :
+                <div className="pricing">{`$${integration.pricing.monthly}/mo`}</div> :
                 ""
             }
           </div>
